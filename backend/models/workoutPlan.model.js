2ed37fc862eee3c7102191ebcad79ede0b69deb1
Kulanika <kulanikag2017@gmail.com>
2021-08-17 23:06:44 +0530

    CreateWorkOutModel AB#33

---
+++ backend/models/workoutPlan.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+const{Schema} = mongoose
+
+const workoutPlanSchema = mongoose.Schema({
+    name:  String, 
+    level: String,
+    area: String,
+    description:   String,
+    detailedDescription: String,
+    price: Number,
+    imgUrl: String,
+    createdate: { type: Date, default: Date.now }
+    
+  });
+
+  module.exports = mongoose.model("WorkoutPlanModel",workoutPlanSchema)

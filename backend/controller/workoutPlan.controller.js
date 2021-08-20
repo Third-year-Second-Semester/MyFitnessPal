33b0961cef3907c595fa977a2dc4986ce47ee63e
Kulanika <kulanikag2017@gmail.com>
2021-08-20 18:52:12 +0530

    AB#52 :Modified Controller to get workout plans

--- backend/controller/workoutPlan.controller.js
+++ backend/controller/workoutPlan.controller.js
@@ -30,7 +30,33 @@ const updateWorkOutPlan = async (req, res) => {
     }
 }
 
+//retrieve all workout plans
+const getAlLWorkoutPlans = async (req, res) => {
+    await WorkOutPlanModel.find({})
+        .then(data => {
+            res.status(200).send({ data: data });
+        })
+        .catch(error => {
+            res.status(500).send({ error: error.message });
+        });
+}
+
+//retrieve a workout plan by id
+const getWorkoutPlan = async(req,res) =>{
+    if(req.params && req.params.id){
+        await WorkOutPlanModel.findById(req.params.id)
+        .then(data =>{
+            res.status(200).send({data:data});
+        })
+        .catch(error =>{
+            res.status(500).send({error:error.message});
+        });
+    }
+}
+
 module.exports = {
     createWorkOutPlan,
-    updateWorkOutPlan
+    updateWorkOutPlan,
+    getAlLWorkoutPlans,
+    getWorkoutPlan
 };

0bfead7b1eefa1f99712a2339553b8fbbbcdddcc
kavishka721 <IT19156248@my.sliit.lk>
2021-09-21 14:40:05 +0530

    blog report admin view AB#79

--- backend/router/blogpost.router.js
+++ backend/router/blogpost.router.js
@@ -4,14 +4,13 @@ const router = express.Router();
 const fileUpload = require("../MiddlewearBlogpost/blogpostImageupload");
 const blogpostController = require("../controller/blogpost.controller");
 const commentController = require('../controller/comments.controller');
-const comment = require("../models/comments.model");
 
 module.exports = function () {
 
    
    router.post('/create', fileUpload.single('image'),blogpostController.createBlogPost);
-   router.put("/update/:id", blogpostController.updateBlogPost);
-   router.delete("/delete/:id", blogpostController.deleteBlogPost);
+   router.put("/update/:id",fileUpload.single('image'), blogpostController.updateBlogPost);
+   router.delete("/delete/:id",blogpostController.deleteBlogPost);
    router.get("/:id", blogpostController.getaBlogPost);
    router.get("/", blogpostController.getAllBlogPostsDetails);
 

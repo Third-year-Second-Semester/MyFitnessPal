2481f9e52230618b81efd1110b984862888b8f28
kavishka721 <IT19156248@my.sliit.lk>
2021-09-24 23:51:16 +0530

    blog search bar AB#80

---
+++ Frontend/myfitnesspal/src/component/BlogPost/admin/viewblogadmin.js
@@ -0,0 +1,215 @@
+import axios from "axios";
+import { Button } from "bootstrap";
+import React, { Component } from "react";
+import Navbar from "../../NavBar/Navbar.component";
+import "../user/viewblogpost.css";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
+
+class ViewBlogAdmin extends Component {
+  constructor(props) {
+    super(props);
+    this.commentSubmit = this.commentSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+
+    this.state = {
+      blogs: [],
+      allComments: [],
+      comments: [],
+      date: "",
+      blogid: "",
+      username: "",
+      email: "",
+      comment: "",
+      posteddate: "",
+      alertMsg: "",
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  componentDidMount(e, id) {
+    axios
+      .get(`http://localhost:8084/blogposts/${this.props.match.params.id}`)
+      .then((response) => {
+        this.setState({ blogs: response.data.data });
+        this.setState({ date: this.state.blogs.date });
+
+        // console.log(response.data.data._id);
+        // console.log(this.props.match.params.id);
+      });
+
+    axios
+      .get(
+        `http://localhost:8084/blogposts/comments/${this.props.match.params.id}`
+      )
+      .then((response) => {
+        // console.log(response.data.data);
+        this.setState({ comments: response.data.data });
+        ///console.log(this.state.comments);
+      });
+  }
+
+  componentDidUpdate(e) {
+    axios
+      .get(
+        `http://localhost:8084/blogposts/comments/${this.props.match.params.id}`
+      )
+      .then((response) => {
+        this.setState({ comments: response.data.data });
+      });
+  }
+
+  handleDelete(id) {
+    confirmAlert({
+      title: "Confirm to Delete",
+      message: "Are you sure to delete this blog post..?",
+      buttons: [
+        {
+          label: "Yes",
+          onClick: () =>
+            axios
+              .delete(`http://localhost:8084/blogposts/delete/${id}`)
+              .then((response) => {
+                confirmAlert({
+                  title: "Blog post successfully deleted!!",
+                  buttons: [{ label: "OK" }],
+                });
+
+                window.location = "../adminbloglist";
+              })
+              .catch((error) => {
+                alert(error.message);
+              }),
+        },
+        {
+          label: "No",
+        },
+      ],
+    });
+  }
+
+  //===================================================================================================================
+  commentSubmit = (e) => {
+    e.preventDefault();
+
+    let newComment = {
+      blogid: this.props.match.params.id,
+      username: this.state.username,
+      email: this.state.email,
+      comment: this.state.comment,
+      posteddate: Date.now(),
+    };
+
+    axios
+      .post("http://localhost:8084/blogposts/save", newComment)
+      .then((response) => {
+        console.log("data  " + response.data);
+        if (response.data == null) {
+          this.setState({ alertMsg: "Username is already existing" });
+        } else {
+          this.setState({ alertMsg: "Comment is posted!!" });
+        }
+      })
+      .catch((err) => {
+        alert("error = " + err);
+      });
+
+    // if (this.state.allComments.username != this.state.username) {
+
+    // } else {
+    //   alert("This username has been used already!!");
+    // }
+  };
+  //=========================================================================================================================
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+
+        <div className="container">
+          <br />
+          <h2 className="blogtitle">{this.state.blogs.title}</h2>
+          <br />
+          <img
+            className="blogimg"
+            src={`http://localhost:8084/${this.state.blogs.image}`}
+          />
+          <br />
+          <br />
+          <textarea
+            className="bodycontent"
+            onInput='this.style.height = "";this.style.height = this.scrollHeight + "px"'
+            value={this.state.blogs.bodyContent}
+          >
+            {this.state.blogs.bodyContent}
+          </textarea>
+          {/* <div className="bodycontent">{this.state.blogs.bodyContent}</div> */}
+          <br />
+          <p className="date">{this.state.date.substr(0, 10)}</p>
+          <br />
+          <br />
+
+          <div className="commentsection">
+            <div className="commentheader">Comment Section</div>
+
+            <br />
+
+            <div>
+              <p>
+                <b>Comments</b>
+              </p>
+              {this.state.comments.length > 0 &&
+                this.state.comments.map((comments) => (
+                  <div className="" style={{ width: "500px" }}>
+                    <ul className="card-body">
+                      <li>
+                        <strong class="text-success">
+                          {comments.username}
+                        </strong>
+                        <span className="text-muted">
+                          <small className="text-muted right">
+                            {comments.posteddate.substring(0, 10)}
+                          </small>
+                        </span>
+                        <p className="">{comments.comment}</p>
+                      </li>
+                    </ul>
+                  </div>
+                ))}
+            </div>
+          </div>
+
+          <div className="btnset">
+            <a
+              href={`/editblog/${this.state.blogs._id}`}
+              className="btn btn-success"
+            >
+              EDIT
+            </a>
+
+            {"  "}
+
+            <button
+              //href={`/deleteblog/${blogs._id}`}
+              type="button"
+              className="btn btn-danger"
+              onClick={() => {
+                this.handleDelete(this.state.blogs._id);
+              }}
+            >
+              DELETE
+            </button>
+            <br />
+            <br />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ViewBlogAdmin;
--- Frontend/myfitnesspal/src/component/BlogPost/user/viewAllBlogs.js
+++ Frontend/myfitnesspal/src/component/BlogPost/user/viewAllBlogs.js
@@ -9,8 +9,13 @@ class ViewAllBlogs extends Component {
     super(props);
     this.state = {
       blogs: [],
-      data: []
+      data: [],
+      search: "",
     };
+
+    this.handleSearch = this.handleSearch.bind(this);
+    this.renderBlogs = this.renderBlogs.bind(this);
+    this.searchBlog = this.searchBlog.bind(this);
   }
 
   componentDidMount(e) {
@@ -18,10 +23,76 @@ class ViewAllBlogs extends Component {
       this.setState({ blogs: response.data.data });
       console.log(response.data.data);
     });
+  }
 
+  // handle search function implementation
+  handleSearch(e) {
+    this.setState({
+      search: e.target.value,
+    });
+  }
+
+  //search blog function implementation
+  searchBlog(e) {
+    e.preventDefault();
+    this.renderBlogs(this.state.search);
+  }
+
+  // render blog function implementation
+  renderBlogs(blogs) {
+    const { search } = this.state;
+
+    // if (
+    //   search !== "" &&
+    //   blogs.title.toLowerCase().indexOf(search.toLowerCase()) === -1
+    // ) {
+    //   return null;
+    // }
+
+    // return statement of the function
+    return (
+      <div>
+        <div className="card mb-3" style={{ maxWidth: "1200px" }}>
+          <div className="row g-0" key={blogs.id}>
+            <div className="col-md-4">
+              <img
+                src={`http://localhost:8084/${blogs.image}`}
+                className="img-fluid rounded-start"
+                alt=""
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">{blogs.title}</h5>
+                <p className="card-text">
+                  {blogs.bodyContent.substring(0, 250)}
+                </p>
+                <p className="card-text">
+                  <small className="text-muted">
+                    Posted on: {blogs.date.substring(0, 10)}
+                  </small>
+                </p>
+                <a
+                  href={`/blog/${blogs._id}`}
+                  className="btn btn-primary"
+                  //onClick={(e) => this.viewBlog(e, item._id)}
+                >
+                  Read More..
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   render() {
+    const { search } = this.state;
+    const filteredBlogs = this.state.blogs.filter((blogpost) => {
+      return blogpost.title.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+    });
+
     return (
       <div>
         <Navbar />
@@ -32,43 +103,30 @@ class ViewAllBlogs extends Component {
         </div>
 
         <div className="container">
-          {this.state.blogs.length > 0 &&
-            this.state.blogs.map((blogs) => (
-              // <div key={index} className="card mb-3">
+          <br />
+          {/* search bar */}
 
-              //</div>
+          
+            <input
+              class="form-control me-2 searchbar"
+              type="search"
+              value={this.state.search}
+              onChange={this.handleSearch}
+              placeholder="Search Blog"
+              aria-label="Search"
+            />
+          
+          <br />
 
-              <div className="card mb-3" style={{ maxWidth: "1200px" }}>
-                <div className="row g-0" key={blogs.id}>
-                  <div className="col-md-4">
-                    <img
-                          src={`http://localhost:8084/${blogs.image}`}
-                          className="img-fluid rounded-start"
-                          alt=""
-                        />
-                  </div>
-                  <div className="col-md-8">
-                    <div className="card-body">
-                      <h5 className="card-title">{blogs.title}</h5>
-                      <p className="card-text">
-                        {blogs.bodyContent.substring(0, 250)}
-                      </p>
-                      <p className="card-text">
-                        <small className="text-muted">Posted on: {blogs.date.substring(0,10)}</small>
-                      </p>
-                      <a
-                        href={`/blog/${blogs._id}`}
-                        className="btn btn-primary"
-                        //onClick={(e) => this.viewBlog(e, item._id)}
-                      >
-                        Read More..
-                      </a>
-                      
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
+          <div className="card mb-3">
+            {filteredBlogs.map((blogpost) => {
+              return this.renderBlogs(blogpost);
+            })}
+
+            {/* {this.state.blogs.map((blogpost) => {
+              return this.renderBlogs(blogpost);
+            })} */}
+          </div>
         </div>
       </div>
     );
